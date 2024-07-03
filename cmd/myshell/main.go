9ec5a8ded7676f99e226536261791a3d0df90bdb
Georg Stockinger <gstockinger@a2mac1.com>
2024-07-03 15:02:31 +0200

    exit

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,21 +4,54 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
+
+type Command int
+
+const (
+	exit Command = iota
+	unknown
+)
+
+func determine_command(input string) Command {
+	match, _ := regexp.MatchString("^(exit)\\s.*\\d", input)
+	if match {
+		return exit
+	}
+	return unknown
+}
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		var command, err = bufio.NewReader(os.Stdin).ReadString('\n')
+		var input, err = bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			os.Stderr.WriteString("reading command")
 			continue
 		}
-		command = strings.Replace(command, "\n", "", -1)
+		input = strings.Replace(input, "\n", "", -1)
+		var command = determine_command(input)
+
 		switch command {
+		case exit:
+			var split = strings.Split(input, " ")
+			if len(split) < 2 {
+				var formatted = fmt.Sprintf("Input not understood: '%s'\n", input)
+				fmt.Fprint(os.Stdout, formatted)
+				continue
+			}
+			var exit_code, err = strconv.Atoi(split[1])
+			if err != nil {
+				var formatted = fmt.Sprintf("Input not understood: '%s'\n", input)
+				fmt.Fprint(os.Stdout, formatted)
+				continue
+			}
+			os.Exit(exit_code)
 		default:
-			var formatted = fmt.Sprintf("%s: command not found\n", command)
+			var formatted = fmt.Sprintf("%s: command not found\n", input)
 			fmt.Fprint(os.Stdout, formatted)
 		}
 	}
