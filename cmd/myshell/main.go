1080ef19a526c733b19c6ec59c9efe1d5c71cf91
Georg Stockinger <gstockinger@a2mac1.com>
2024-07-03 14:19:13 +0200

    first

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"bufio"
-	// Uncomment this block to pass the first stage
-	// "fmt"
+	"fmt"
 	"os"
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
+	fmt.Fprint(os.Stdout, "$ ")
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
