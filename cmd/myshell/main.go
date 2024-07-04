4a1eda0beddd73159ded41cd5819d78dd1d85368
Georg Stockinger <gstockinger@a2mac1.com>
2024-07-04 07:20:05 +0200

    handle external

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,121 +5,162 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
-	"regexp"
 	"strconv"
 	"strings"
 )
 
-type Command string
+type Builtin string
 
 const (
-	exit         Command = "exit"
+	exit         Builtin = "exit"
 	echo                 = "echo"
 	type_builtin         = "type"
 	unknown              = "unknown"
 )
 
-var command_map map[Command]string = map[Command]string{exit: "^(exit)\\s.*\\d", echo: "^(echo).*", type_builtin: "^(type).*"}
-
-func determine_command(input string) Command {
-	var match bool
-
-	for k, v := range command_map {
-		match, _ = regexp.MatchString(v, input)
-		if match {
-			return k
-		}
+func handle_external(exe string, args []string) {
+	cmd_args := append([]string{filepath.Base(exe)}, args...)
+	cmd := exec.Command(exe, cmd_args[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("%s: command not found: %s\n", exe, err)
 	}
-	return unknown
 }
 
-func find_in_path(exe string) (string, error) {
+func parse_builtin(exe string) (Builtin, error) {
+	switch exe {
+	case string(exit):
+		return exit, nil
+	case string(echo):
+		return echo, nil
+	case string(type_builtin):
+		return type_builtin, nil
+	}
+	return unknown, errors.New("Not a builtin")
+}
+
+func find_executable(exe string) (string, error) {
 	var env_path = os.Getenv("PATH")
 	var paths = strings.Split(env_path, ":")
 	for _, path := range paths {
 		var formatted = fmt.Sprintf("%s/%s", path, exe)
 		var matches, err = filepath.Glob(formatted)
 		if err == nil && len(matches) > 0 {
-			var formatted = fmt.Sprintf("%s is %s\n", exe, matches[0])
-			return formatted, nil
+			return matches[0], nil
 		}
 	}
 	return "", errors.New("executable not found in PATH")
 }
 
-func find_in_builtins(exe string) (string, error) {
-	if _, ok := command_map[Command(exe)]; ok {
-		var formatted = fmt.Sprintf("%s is a shell builtin\n", exe)
-		return formatted, nil
+func handle_builtin(builtin Builtin, args []string) {
+	switch builtin {
+	case exit:
+		handle_exit(args)
+	case echo:
+		handle_echo(args)
+	case type_builtin:
+		handle_type(args)
+	default:
+		return
 	}
-	return "", errors.New("executable not found in builtins")
 }
 
-func handle_type(input string) {
-	var split = strings.Split(input, " ")
-	var exe = split[1]
-	var formatted string
+func handle_type(args []string) {
 	var err error
+	var exe string
 
-	formatted, err = find_in_builtins(exe)
+	if len(args) == 0 || len(args) > 1 {
+		fmt.Fprint(os.Stdout, "Usage: type 'command'\n")
+		return
+	}
+
+	_, err = parse_builtin(args[1])
 	if err == nil {
+		var formatted = fmt.Sprintf("%s is a shell builtin\n", args[1])
 		fmt.Fprint(os.Stdout, formatted)
 		return
 	}
 
-	formatted, err = find_in_path(exe)
+	exe, err = find_executable(args[1])
 	if err == nil {
+		var formatted = fmt.Sprintf("%s is a %s\n", args[1], exe)
 		fmt.Fprint(os.Stdout, formatted)
 		return
 	}
-	formatted = fmt.Sprintf("%s: not found\n", exe)
-	fmt.Fprint(os.Stdout, formatted)
+	exe = fmt.Sprintf("%s: not found\n", args[1])
+	fmt.Fprint(os.Stdout, exe)
 }
 
-func handle_exit(input string) {
-	var split = strings.Split(input, " ")
-	if len(split) < 2 {
-		var formatted = fmt.Sprintf("Input not understood: '%s'\n", input)
+func handle_exit(args []string) {
+	var exit_code int
+	var err error
+	if len(args) > 1 {
+		var formatted = fmt.Sprintf("Input not understood: exit '%s'\n", args)
 		fmt.Fprint(os.Stdout, formatted)
 		return
 	}
-	var exit_code, err = strconv.Atoi(split[1])
-	if err != nil {
-		var formatted = fmt.Sprintf("Input not understood: '%s'\n", input)
-		fmt.Fprint(os.Stdout, formatted)
-		return
+	if len(args) == 0 {
+		exit_code = 0
+	} else {
+		exit_code, err = strconv.Atoi(args[0])
+		if err != nil {
+			var formatted = fmt.Sprintf("Input not understood: exit '%s'\n", args)
+			fmt.Fprint(os.Stdout, formatted)
+			return
+		}
 	}
 	os.Exit(exit_code)
 }
 
-func handle_echo(input string) {
-	var split = strings.Split(input, " ")
-	fmt.Fprint(os.Stdout, strings.Join(split[1:], " "))
+func handle_echo(args []string) {
+	if len(args) > 0 {
+		fmt.Fprint(os.Stdout, strings.Join(args, " "))
+	}
 	fmt.Fprint(os.Stdout, "\n")
 }
 
 func main() {
+	var input_command string
+	var input string
+	var err error
+	var builtin Builtin
+	var exe string
+
 	for {
+		var args []string
 		fmt.Fprint(os.Stdout, "$ ")
-		var input, err = bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err = bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			os.Stderr.WriteString("reading command")
 			continue
 		}
 		input = strings.Replace(input, "\n", "", -1)
-		var command = determine_command(input)
-
-		switch command {
-		case exit:
-			handle_exit(input)
-		case echo:
-			handle_echo(input)
-		case type_builtin:
-			handle_type(input)
-		default:
-			var formatted = fmt.Sprintf("%s: command not found\n", input)
-			fmt.Fprint(os.Stdout, formatted)
+		if input == "" {
+			continue
 		}
+		var cmd_args = strings.Split(input, " ")
+		input_command = cmd_args[0]
+		if len(cmd_args) > 1 {
+			args = cmd_args[1:]
+		} else {
+			args = []string{}
+		}
+
+		builtin, err = parse_builtin(input_command)
+		if err == nil {
+			handle_builtin(builtin, args)
+			continue
+		}
+		exe, err = find_executable(input_command)
+		if err == nil {
+			handle_external(exe, args)
+			continue
+		}
+		var formatted = fmt.Sprintf("%s: command not found\n", input)
+		fmt.Fprint(os.Stdout, formatted)
 	}
 }
